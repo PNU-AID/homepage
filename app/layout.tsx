4daedf08e3cc536786c9313ba2381a8234b33ca4
KimCookieYa <min49590@gmail.com>
2024-06-30 00:48:29 +0900

    feat: html ko

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className="scroll-smooth">
+        <html lang="ko" className="scroll-smooth">
             <body className={`${notoSansKr.className} ${balooBhai.variable}`}>
                 <Suspense>
                     <Navi />
