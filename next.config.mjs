e27d5473122e15fde0e5f773c3eacc1e08c754d9
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-06-30 18:31:47 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'developement';
 
 const nextConfig = {
     reactStrictMode: true,
-    basePath: isProduction ? '/aid-web-nextjs' : '', // nextjs public data 오류 해결
+    basePath: isDevelopment ? '' : '/aid-web-nextjs', // nextjs public data 오류 해결
     images: { // GitHub PAge는 SSG 렌더링만 가능함 -> Nextjs Image 최적화 비활성화
         unoptimized: true
     }
