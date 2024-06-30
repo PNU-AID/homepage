6e1dd7cc80e703d18b6d59794bbcb2e2803d5e32
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-06-30 16:52:46 +0900

    mobile UI 수정

--- app/component/AidTop.tsx
+++ app/component/AidTop.tsx
@@ -41,19 +41,19 @@ export default function AidTop() {
     return (
         <section className="" id="about_us">
             <div className="flex flex-col gap-y-2 md:justify-center">
-                <h1 className="text-9xl font-black">AID</h1>
-                <h6 className="text-2xl">AI Engineering & Research in PNU</h6>
+                <h1 className="text-9xl font-black md:text-8xl">AID</h1>
+                <h6 className="text-2xl md:font-bold md:text-lg">AI Engineering & Research in PNU</h6>
                 {lang == 'ko' ? (
                     <p className="text-sm text-gray-500">
                         AID는 AI 개발에 관심있는 사람들이 함께 스터디와 세미나를 진행하고, 대회와 프로젝트를 하며
                         발전하는 동아리입니다.
-                        <br />
+                        <br className='md:hidden' />
                         우리는 같은 진로를 꿈꾸는 사람들과의 네트워크를 넓히고 개인 및 단체의 실력 발전을 목표로 하고
                         있습니다.
                         <br />
                         <br />
                         본 동아리는 부산대학교 AI 동아리 조준수 교수님의 지도 아래,
-                        <br />
+                        <br className='md:hidden' />
                         부산대학교 정보컴퓨터공학 학생들을 중심으로 2022년 1학기에 설립되어 다양한 학생들이 함께 하고
                         있습니다.
                     </p>
