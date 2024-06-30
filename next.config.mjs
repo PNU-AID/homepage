0dbe9f3f9a16f9fa614161ec537c3157503204a5
DongHoon Lee <67902252+bluelemon61@users.noreply.github.com>
2024-06-30 18:51:37 +0900

    prettier

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,10 @@
 const nextConfig = {
     reactStrictMode: true,
     basePath: '/aid-web-nextjs', // nextjs public data 오류 해결
-    images: { // GitHub PAge는 SSG 렌더링만 가능함 -> Nextjs Image 최적화 비활성화
-        unoptimized: true
-    }
+    images: {
+        // GitHub PAge는 SSG 렌더링만 가능함 -> Nextjs Image 최적화 비활성화
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
